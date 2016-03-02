ec3aa518ea6672d53ba257e10b84f5b563565c61
ToniRib <tonimarierib@gmail.com>
2016-03-02 11:38:24 -0700

    Add some controller specs for welcome and dashboard

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe WelcomeController, type: :controller do
+  describe "GET #show" do
+    it "responds with successful 200 HTTP status code" do
+      get :show
+
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+end
