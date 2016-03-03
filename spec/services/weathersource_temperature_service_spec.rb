190a67752a95c29c481c5878ca18686b1a4521df
ToniRib <tonimarierib@gmail.com>
2016-03-03 15:49:48 -0700

    Fix temperature weather source service test to use set dates and locations

--- spec/services/weathersource_temperature_service_spec.rb
+++ spec/services/weathersource_temperature_service_spec.rb
@@ -3,22 +3,12 @@ require "rails_helper"
 RSpec.describe WeathersourceTemperatureService do
   describe ".update_workouts_with_temperature" do
     it "updates workouts with temperature for that date, time, and location" do
-      user = User.create(user_params)
-
-      VCR.use_cassette("load_all_workouts") do
-        MmfWorkoutAggregateService.load_workouts(user.id)
+      user = create(:user_with_workouts_and_routes)
+      user.workouts.each do |w|
+        w.update_temperature(nil)
+        w.update_attribute(:starting_datetime, DateTime.new(2015,7,11,12,0,0))
+        w.update_attribute(:starting_datetime, DateTime.new(2016,2,11,12,0,0))
       end
-
-      VCR.use_cassette("load_all_routes") do
-        MmfRouteService.load_routes(user.id)
-      end
-
-      VCR.use_cassette("update_zipcodes") do
-        GoogleGeocoderService.update_routes_with_zipcodes(user.id)
-      end
-
-      workout_ids = Workout.pluck(:id)[2..-1]
-      Workout.destroy(workout_ids)
 
       expect(Workout.count).to eq(2)
       expect(Workout.pluck(:temperature)).to eq([nil, nil])
@@ -27,7 +17,7 @@ RSpec.describe WeathersourceTemperatureService do
         WeathersourceTemperatureService.update_workouts_with_temperature(user.id)
       end
 
-      expect(Workout.pluck(:temperature)).to eq([56.9, 71.7])
+      expect(Workout.pluck(:temperature)).to eq([35.6, 35.6])
     end
   end
 end
