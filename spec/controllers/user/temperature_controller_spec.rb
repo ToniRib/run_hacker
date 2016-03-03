750cc47b01ac49c447b7f8e7a207783b46091bb9
ToniRib <tonimarierib@gmail.com>
2016-03-03 15:58:41 -0700

    Add simple controller specs for workouts and temperature

---
+++ spec/controllers/user/temperature_controller_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe User::TemperatureController, type: :controller do
+  describe "GET #index" do
+    it "responds with successful 200 HTTP status code" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      get :index
+
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+end
