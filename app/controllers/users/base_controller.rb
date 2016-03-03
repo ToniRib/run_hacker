1c4959709015c9d5c0a9e075918c8b9167c024a4
ToniRib <tonimarierib@gmail.com>
2016-03-03 09:13:28 -0700

    Ensure temperature controller is nested under the users base controller

--- app/controllers/users/base_controller.rb
+++ app/controllers/users/base_controller.rb
@@ -2,7 +2,9 @@ class Users::BaseController < ApplicationController
   before_action :require_current_user
 
   def require_current_user
-    flash[:error] = "Must be logged in to view that page"
-    redirect_to root_path unless current_user
+    unless current_user
+      flash[:error] = "Must be logged in to view that page"
+      redirect_to root_path
+    end
   end
 end
