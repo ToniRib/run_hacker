54d673442ffc0070c288bed188d9a651be051941
ToniRib <tonimarierib@gmail.com>
2016-03-05 09:28:36 -0700

    Redirect to 404 if user does not own workout or workout does not exist

--- app/controllers/user/workouts_controller.rb
+++ app/controllers/user/workouts_controller.rb
@@ -1,10 +1,15 @@
 class User::WorkoutsController < User::BaseController
+  before_action :check_if_workout_exists, only: [:show]
+  before_action :check_owner_of_workout, only: [:show]
+
   def index
     @workouts = current_user.workouts.includes(:route, :location)
   end
 
   def show
-    @workout = Workout.find(params[:id])
+    @workout = Rails.cache.fetch("workout-and-location-#{params[:id]}") do
+      Workout.includes(:location).find(params[:id])
+    end
 
     if @workout.has_time_series
       @time_series = Rails.cache.fetch("workout-#{@workout.map_my_fitness_id}") do
@@ -15,5 +20,23 @@ class User::WorkoutsController < User::BaseController
       flash["error"] = "Workout does not have a time series to display"
       redirect_to workouts_path
     end
+  end
+
+  private
+
+  def check_owner_of_workout
+    render file: "public/404" unless workout_belongs_to_current_user
+  end
+
+  def check_if_workout_exists
+    render file: "public/404" unless workout_exists
+  end
+
+  def workout_exists
+    Workout.exists?(params[:id])
+  end
+
+  def workout_belongs_to_current_user
+    Workout.find(params[:id]).user.id == current_user.id
   end
 end
