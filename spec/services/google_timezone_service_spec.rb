fd1663bdd47cea813e0f02b0904dd497bea43a16
ToniRib <tonimarierib@gmail.com>
2016-03-07 08:40:27 -0700

    Simply calls to the google timezone service in its test

--- spec/services/google_timezone_service_spec.rb
+++ spec/services/google_timezone_service_spec.rb
@@ -3,30 +3,18 @@ require "rails_helper"
 RSpec.describe GoogleTimezoneService do
   describe ".update_routes_with_timezones" do
     it "updates the route's associated location with a timezone" do
-      user = User.create(user_params)
-
-      VCR.use_cassette("load_all_workouts") do
-        MmfWorkoutAggregateService.load_workouts(user.id)
-      end
-
-      VCR.use_cassette("load_all_routes") do
-        MmfRouteService.load_routes(user.id)
-      end
-
-      expect(Location.count).to eq(16)
-      expect(Location.pluck(:local_timezone)).to eq(Array.new(16, nil))
+      workout = create(:workout)
+      user = workout.user
+      location = workout.location
+      location.update_attribute(:local_timezone, nil)
 
       VCR.use_cassette("update_timezones") do
         GoogleTimezoneService.update_routes_with_timezones(user.id)
       end
 
-      timezones = Location.pluck(:local_timezone)
+      timezone = Location.pluck(:local_timezone).first
 
-      expect(timezones.include?("America/Los_Angeles")).to be true
-      expect(timezones.include?("America/Denver")).to be true
-      expect(timezones.include?("America/Anchorage")).to be true
-      expect(timezones.include?("America/New_York")).to be true
-      expect(timezones.include?("America/Chicago")).to be true
+      expect(timezone).to eq("America/Denver")
     end
   end
 end
