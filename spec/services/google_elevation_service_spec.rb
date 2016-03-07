5582f3dfe8838f4bec83929a8434a2b6ff233302
ToniRib <tonimarierib@gmail.com>
2016-03-07 08:37:30 -0700

    Simplify call to google elevation service in its test

--- spec/services/google_elevation_service_spec.rb
+++ spec/services/google_elevation_service_spec.rb
@@ -3,17 +3,16 @@ require "rails_helper"
 RSpec.describe GoogleElevationService do
   describe ".update_routes_with_elevation" do
     it "updates routes with cooresponding elevation based on lat lng" do
-      user = create(:user_with_workouts_and_routes)
-      user.routes.each { |r| r.update_elevation(nil) }
-
-      expect(Route.count).to eq(2)
-      expect(Route.pluck(:elevation)).to eq([nil, nil])
+      workout = create(:workout)
+      route = workout.route
+      route.update_elevation(nil)
+      user = workout.user
 
       VCR.use_cassette("update_elevations") do
         GoogleElevationService.update_routes_with_elevation(user.id)
       end
 
-      expect(Route.pluck(:elevation)).to eq([1655.54797363281, 1655.54797363281])
+      expect(Route.pluck(:elevation)).to eq([1655.54797363281])
     end
   end
 end
