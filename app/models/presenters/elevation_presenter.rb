9d43fc6496981a25652b3b60b44086af42263ba6
ToniRib <tonimarierib@gmail.com>
2016-03-09 15:02:41 -0700

    Another upload for heroku to try out elevation chart

--- app/models/presenters/elevation_presenter.rb
+++ app/models/presenters/elevation_presenter.rb
@@ -8,29 +8,29 @@ class Presenters::ElevationPresenter < SimpleDelegator
 
   def distance_elevation_and_total_time
     data = cached_workouts
-             .where("elevation IS NOT NULL")
+             .has_elevations
              .has_elapsed_time
-             .select(:distance, "routes.elevation", :elapsed_time, :route_id)
+             .select(:distance, :elevation, :elapsed_time, :route_id)
 
     create_highcharts_data(data, :elapsed_time_in_minutes)
   end
 
   def distance_elevation_and_average_speed
     data = cached_workouts
-             .where("elevation IS NOT NULL")
+             .has_elevations
              .has_average_speed
-             .select(:distance, "routes.elevation", :average_speed, :route_id)
+             .select(:distance, :elevation, :average_speed, :route_id)
 
     create_highcharts_data(data, :average_speed_in_mph)
   end
 
   def distance_elevation_and_time_spent_resting
     data = cached_workouts
-             .where("elevation IS NOT NULL")
+             .has_elevations
              .has_elapsed_time
              .has_active_time
              .where("elapsed_time - active_time > 0")
-             .select(:distance, "routes.elevation", :elapsed_time, :active_time, :route_id)
+             .select(:distance, :elevation, :elapsed_time, :active_time, :route_id)
 
     create_highcharts_data(data, :time_spent_resting_in_minutes)
   end
