06fc1d85b06133ab7b376c0f8080dc4dad271afa
ToniRib <tonimarierib@gmail.com>
2016-03-09 09:16:03 -0700

    Filter by min & max distance (but not both)

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-
-
   $('#location-single-select').on('change', function() {
     var $rows = $('.clickable-row');
     var selectedLocation = $('#location-single-select :selected').text();
@@ -16,4 +14,56 @@ $(document).ready(function(){
       }
     });
   });
+
+  $('#minimum_distance').on("keypress",function(e) {
+    if(e.keyCode === 13) {
+      filterByMinimumDistance();
+    }
+  });
+
+  $('#minimum_distance').on("change",function(e) {
+    filterByMinimumDistance();
+  });
+
+  $('#maximum_distance').on("keypress",function(e) {
+    if(e.keyCode === 13) {
+      filterByMaximumDistance();
+    }
+  });
+
+  $('#maximum_distance').on("change",function(e) {
+    filterByMaximumDistance();
+  });
 });
+
+function filterByMinimumDistance() {
+  var $rows = $('.clickable-row');
+  var minimumDistance = parseFloat($('#minimum_distance').val());
+
+  $rows.each(function(index, row) {
+    var $row = $(row);
+    var $rowDistance = parseFloat($row.find(".workout-distance").text());
+
+    if ($rowDistance >= minimumDistance) {
+      $row.show();
+    } else {
+      $row.hide();
+    }
+  });
+}
+
+function filterByMaximumDistance() {
+  var $rows = $('.clickable-row');
+  var maximumDistance = parseFloat($('#maximum_distance').val());
+
+  $rows.each(function(index, row) {
+    var $row = $(row);
+    var $rowDistance = parseFloat($row.find(".workout-distance").text());
+
+    if ($rowDistance <= maximumDistance) {
+      $row.show();
+    } else {
+      $row.hide();
+    }
+  });
+}
--- app/assets/javascripts/sort.js
+++ app/assets/javascripts/sort.js
@@ -61,7 +61,7 @@ $(document).ready(function() {
       $("#workouts-table").append("<tr class='clickable-row' id='" +
                                   tableContents[i].id + "' data-link='" +
                                   tableContents[i].dataLink + "'><td>" +
-                                  tableContents[i].date + "</td><td>" +
+                                  tableContents[i].date + "</td><td class='workout-distance'>" +
                                   tableContents[i].distance + "</td><td>" +
                                   tableContents[i].speed + "</td><td>" +
                                   tableContents[i].time + "</td><td>" +
