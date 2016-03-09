7acb245ac0e0f4789c366a91cdd2316ffaf9bf7f
ToniRib <tonimarierib@gmail.com>
2016-03-08 17:21:55 -0700

    Add test for clicking on a workout from the list

--- spec/features/user_views_a_specific_workout_spec.rb
+++ spec/features/user_views_a_specific_workout_spec.rb
@@ -31,6 +31,33 @@ RSpec.feature "User views a specific workout" do
     expect(page).to have_content("72.6 ft")
   end
 
+  scenario "workout clicked on from workout listing", js: true do
+    user = User.create(user_params)
+    map_my_fitness_id = "542541966"
+    workout = create(:workout, user: user, map_my_fitness_id: map_my_fitness_id)
+
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user)
+      .and_return(user)
+
+    visit workouts_path
+
+    find("#workout-#{map_my_fitness_id}").click
+
+    expect(page).to have_css("#google-map")
+    expect(page).to have_content(workout.starting_date_no_time)
+    expect(page).to have_content(workout.starting_time_only)
+    expect(page).to have_content(workout.location.city_and_state)
+    expect(page).to have_content(workout.distance_in_miles)
+    expect(page).to have_content(workout.elapsed_time_in_minutes)
+    expect(page).to have_content(workout.temperature)
+    expect(page).to have_content(workout.calories_burned_in_kcal)
+    expect(page).to have_content(workout.average_speed_in_mph)
+    expect(page).to have_content("Not Available")
+    expect(page).to have_content("5278.22 ft")
+    expect(page).to have_content("5296.0 ft")
+  end
+
   scenario "workout does not have a timeseries available" do
     user = create(:user)
     workout = create(:workout, user: user, has_time_series: false)
